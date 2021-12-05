767ee79a90fcdd45e83317698eb12bfbe5f62c43
mjyinseoul <mjyinseoul@gmail.com>
2021-12-05 16:07:30 +0900

    set cartItemType

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
 
-function App() {
+//Components 
+import Drawer from '@material-ui/core/Drawer'
+import { LinearProgress } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid'
+import { AddShoppingCart } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+
+//styles 
+import { Wrapper } from './App.styles';
+
+//Types 
+export type CartItemType = {
+  id : number;
+  category : string; 
+  description : string;
+  image : string;
+  price : number;
+  title : string;
+  amount : number;
+}
+
+const getProducts = async () : Promise<CartItemType> => 
+  //first await is for converting JSON
+  //second await is for fetching API
+  await (await fetch('https://facestoreapi.com/products')).json()
+
+
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
-
-export default App;
