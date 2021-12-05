f36c6b0243eea2554ba7fa6a735b751323ec91c5
mjyinseoul <mjyinseoul@gmail.com>
2021-12-05 17:22:57 +0900

    create add/remove btn for cart

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -3,12 +3,32 @@ import { FC } from "react"
 
 //Types 
 import { CartItemType } from "../App"
+import { Item } from "../Item/Item"
 
 //Styles
 import { Wrapper } from "./CartItem.styled"
 
-export const CartItem:FC = () => (
-        <div>
-            
-        </div>
+type Props = {
+    item: CartItemType;
+    addToCart: (clickedItem: CartItemType) => void;
+    removeFromCart: (id:number) => void; 
+}
+
+export const CartItem:FC<Props> = ({ item, addToCart, removeFromCart }) => (
+        <Wrapper>
+            <div>
+                <h3>{item.title}</h3>
+                <div className="information">
+                    <p>Price: ${item.price}</p>
+                    <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+                </div>
+                <div className="buttons">
+                    <Button size="small" disableElevation 
+                        variant="contained" onClick={() => removeFromCart(item.id)}>-</Button>
+                    <Button size="small" disableElevation 
+                        variant="contained" onClick={() => addToCart(item)}>+</Button>
+                </div>
+            </div>
+            <img src={item.image} alt={item.title} />
+        </Wrapper>
     )
